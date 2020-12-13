9c259fb6d95cdc5519204d43cde16af1854ef3ef
phibzy <phibzy@gmail.com>
2020-12-13 17:49:18 +1100

    Found bug for pt 2

--- 12/rainRisk.py
+++ 12/rainRisk.py
@@ -42,7 +42,7 @@ def manhattanDistance(inp):
             # we use a negative multiplier. Otherwise we use a positive one.
             # Mod result by number of directions so we have index of current
             # direction.
-            currentDirection = (val // TURN)*TURN_MULTIPLIER[i == "R"]
+            currentDirection += (val // TURN)*TURN_MULTIPLIER[i == "R"]
             currentDirection %= NUM_DIRECTIONS
 
         elif i == "F":
@@ -59,5 +59,60 @@ def manhattanDistance(inp):
     # Get absolute vals of coords, then sum together
     return sum(map(abs, distOrigin)) 
 
+# pt 2: Keep track of boat/waypoint origins
+# Update boat coords as += wayP[0]*val, wayP[1]*val
+def manhattanPt2(inp):
+    boat = [0,0]
+    wayP = [10,1]
+
+    # Some constants for handling turning
+    TURN = 90
+    D_MOD = {"E": (0, 1), "S": (1, -1), "W": (0, -1), "N": (1, 1)}
+
+    # For each instruction and number
+    for i, val in inp:
+
+        # Rotating the waypoint 
+        if i == "L" or i == "R":
+            rVal = (val // TURN) % 4
+            # For rotating, you basically swap coords and then
+            # apply negative rules depending on quadrant
+            wayP = rotatePoint(wayP, rVal, i)
+
+
+        # Move towards waypoint
+        elif i == "F":
+            # We move towards the waypoint X times
+            # So just multiply coords of waypoint and add to boat coords
+            boat[0] += val*wayP[0]
+            boat[1] += val*wayP[1]
+
+        # Moving the waypoint 
+        # Works the same way as pt.1, but this time we move the waypoint
+        else:
+            axis, multiplier = D_MOD[i]
+            wayP[axis] += multiplier*val
+
+    # Get absolute vals of coords, then sum together
+    return sum(map(abs, boat)) 
+
+def rotatePoint(wayP, rVal, i):
+    # For 180 rotations we flip the signs of both wayP coords
+    if rVal == 2:
+        wayP[0] *= -1
+        wayP[1] *= -1
+
+    # ERROR IS IN HERE
+    # Rotating left once is same as rotating right 3 times
+    elif (i == "L" and rVal == 1) or (i == "R" and rVal == 3):
+        wayP[0], wayP[1] = wayP[1]*-1, abs(wayP[0])
+        
+    # Rotating right once is same as rotating left 3 times 
+    elif (i == "R" and rVal == 1) or (i == "L" and rVal == 3):
+        wayP[0], wayP[1] = abs(wayP[1]), wayP[0]*-1
+
+    # For 0 and 360 rotations we don't need to worry (none in input anyway)
+    return wayP
+
 inp = parseInput(Path("./input/puzzle_input").read_text())
-print(manhattanDistance(inp))
+print(manhattanPt2(inp))
--- 12/test.py
+++ 12/test.py
@@ -9,7 +9,7 @@
 
 import unittest
 from pathlib import Path
-from rainRisk import parseInput, manhattanDistance
+from rainRisk import parseInput, manhattanDistance, manhattanPt2, rotatePoint
 
 class testRain(unittest.TestCase):
 
@@ -24,3 +24,23 @@ class testRain(unittest.TestCase):
     def testManhattan(self):
         self.assertEqual(manhattanDistance(self.parsedFiles[0]), 25)
 
+        # 0 manhattan distance tests, just turning around and moving
+        # forward same distance each direction
+        self.assertEqual(manhattanDistance(self.parsedFiles[1]), 0)
+        self.assertEqual(manhattanDistance(self.parsedFiles[2]), 0)
+        self.assertEqual(manhattanDistance(self.parsedFiles[3]), 23)
+
+    def testManhattan(self):
+        self.assertEqual(manhattanPt2(self.parsedFiles[0]), 286)
+
+    def testRotate(self):
+        self.assertEqual(rotatePoint([0,4], 1, "R"), [4,0])
+        self.assertEqual(rotatePoint([0,4], 2, "R"), [0,-4])
+        self.assertEqual(rotatePoint([0,4], 2, "L"), [0,-4])
+        self.assertEqual(rotatePoint([0,4], 3, "R"), [-4,0])
+        self.assertEqual(rotatePoint([0,4], 1, "L"), [-4,0])
+
+        
+        self.assertEqual(rotatePoint([10,4], 1, "R"), [4,-10])
+        self.assertEqual(rotatePoint([4, -10], 1, "R"), [-10, -4])
+        self.assertEqual(rotatePoint([-10,-4], 1, "R"), [-4,10])
