8213b8370550242595314aa2bab08cfaa7214470
phibzy <phibzy@gmail.com>
2020-12-16 11:58:54 +1100

    Pt 1 done

--- 14/docking.py
+++ 14/docking.py
@@ -8,19 +8,22 @@
 """
 
 import re
+from pathlib import Path
 
-# Use dict to store memory address values
-# Sum values at end
+# For each XMask given, create a mask where all Xs are
+# set to 0 (orMask) and one where all Xs are set to one (andMask)
 
-# Create a mask of 1 bits, then set any 0 bits needed
-# We then and this mask with original number, which
-# will give us our target number
+# For each value we need to change, we first OR the value with
+# our orMask - which will set all 1 bits that need to be set
 
-# To parse input, grab mask first.
-    # Set all Xs to 1s
+# We then AND this result with our andMask to set all
+# the 0 bits. We're basically abusing the fact that 1 AND 0 is 0
 
+# We'll then use a dict to store these values at memory address keys
+# so they're easy to rewrite and then sum all values in dict
+
+# To parse input, grab mask first and create our and/orMasks
 # Then grab mem addresses and values on each line after
-
 def parseInput(inp):
     # Split on masks
     # Will have empty string at start so ignore that
@@ -36,7 +39,8 @@ def parseInput(inp):
         # For the mask, we take the first line of the group
         # and change all X's into 1's, before converting it
         # as a binary string into an integer
-        mask = int(re.sub("X", "1", g[0]), 2)
+        orMask   = int(re.sub("X", "0", g[0]), 2)
+        andMask  = int(re.sub("X", "1", g[0]), 2)
 
         # For each remaining line in the group, we want to grab
         # the number between the square brackets (memory address) and
@@ -46,8 +50,33 @@ def parseInput(inp):
         # Then add the group data to the output list,
         # since puzzle input has many groups like this with
         # different masks
-        parsed.append([mask, commands])
+        parsed.append([orMask, andMask, commands])
 
     return parsed
 
+def sumMemory(inp):
+    # Use a dict to keep track of memory addresses
+    # Just have address 0 set to 0 for starters
+    memory = {0: 0}
 
+    for orMask, andMask, commands in inp:
+
+        for address, val in commands:
+            # First, OR with the ormask to add in 
+            # any missing 1s
+            val |= orMask
+
+            # Then AND with the andmask to add in
+            # any 0s that should be there
+            val &= andMask
+
+            # Set the value in memory
+            memory[address] = val
+
+    # Return the sum of all the values in memory
+    return sum(memory.values())
+
+inp = parseInput(Path('./input/puzzle_input').read_text())
+
+# Pt. 1
+print(sumMemory(inp))
--- 14/test.py
+++ 14/test.py
@@ -8,7 +8,7 @@
 """
 
 import unittest
-from docking import parseInput
+from docking import parseInput, sumMemory
 from pathlib import Path
 
 class testDocking(unittest.TestCase):
@@ -17,4 +17,7 @@ class testDocking(unittest.TestCase):
     testData = [ parseInput(f.read_text()) for f in sorted(inputPath.glob("test*")) ]
 
     def testParse(self):
-        self.assertEqual(self.testData[0], [[int("111111111111111111111111111111111101" , 2), [[8, 11], [7, 101], [8, 0]]]])
+        self.assertEqual(self.testData[0], [[int("1000000" , 2), int("111111111111111111111111111111111101", 2), [[8, 11], [7, 101], [8, 0]]]])
+    
+    def testSumMemory(self):
+        self.assertEqual(sumMemory(self.testData[0]), 165)
