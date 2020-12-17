8c4951153d5a0f03e946e095fc921d068efc257d
phibzy <phibzy@gmail.com>
2020-12-17 16:44:55 +1100

    Pt 1 ranges done

--- 16/translation.py
+++ 16/translation.py
@@ -22,11 +22,19 @@ import re
 def parseInput(inp):
     # Split as mentioned above
     rules, yourTix, otherTix = inp.split("\n\n")
+    
+    # Since each field has two groups
+    # We'll get the full range of values for each group
+    group1R = [float('inf'), 0]
+    group2R = [float('inf'), 0]
 
     fieldRules = dict()
     for line in rules.split("\n"):
         field, range1s, range1e, range2s, range2e = re.findall("^(.*): ([0-9]+)\-([0-9]+) or ([0-9]+)\-([0-9]+)$", line)[0]
         fieldRules[field] = [[int(range1s), int(range1e)], [int(range2s), int(range2e)]]
+
+        group1R = [min(group1R[0], int(range1s)), max(group1R[1], int(range1e)) ]
+        group2R = [min(group2R[0], int(range2s)), max(group2R[1], int(range2e)) ]
 
     myVals = list(map(int, yourTix.split("\n")[1].split(",")))
 
@@ -34,7 +42,7 @@ def parseInput(inp):
     for i in range(len(otherTix)):
         otherTix[i] = list(map(int, otherTix[i].split(",")))
 
-    return (fieldRules, myVals, otherTix)
+    return (fieldRules, group1R, group2R, myVals, otherTix)
     
 
 
