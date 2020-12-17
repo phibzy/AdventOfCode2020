9a56d6ab913567a9ab746d32b8b97106ab73ef68
phibzy <phibzy@gmail.com>
2020-12-17 19:57:52 +1100

    Invalid tix handled

--- 16/test.py
+++ 16/test.py
@@ -9,13 +9,38 @@
 
 import unittest
 from pathlib import Path
-from translation import parseInput
+from translation import parseInput, isValid, getErrorRate, getErrorRateN
+from translation import validTix, pt2
 
 class test(unittest.TestCase):
 
     testPath = Path('./input/')
     testInput = [ parseInput(f.read_text()) for f in sorted(testPath.glob("test*")) ]
+    puzzleInput = parseInput(Path(testPath / 'puzzle_input').read_text())
 
     def testParse(self):
         self.assertEqual(self.testInput[0], ({'class': [[1,3],[5,7]], 'row': [[6,11],[33,44]],
             "seat": [[13,40],[45,50]]}, [1,40], [5,50], [7,1,14], [[7,3,47],[40,4,50],[55,2,20],[38,6,12]]))
+
+    def testIsValid(self):
+        self.assertTrue(isValid([23,40], [530,625], 23))
+        self.assertTrue(isValid([23,40], [530,625], 28))
+        self.assertTrue(isValid([23,40], [530,625], 40))
+        self.assertTrue(isValid([23,40], [530,625], 530))
+        self.assertTrue(isValid([23,40], [530,625], 625))
+        self.assertTrue(isValid([23,40], [530,625], 589))
+
+        self.assertFalse(isValid([23,40], [530,625], 12))
+        self.assertFalse(isValid([23,40], [530,625], 50))
+        self.assertFalse(isValid([23,40], [530,625], 670))
+
+    def testErrorRateN(self):
+        self.assertEqual(getErrorRateN(self.testInput[0][0], self.testInput[0][-1]), 71)
+
+    # Test we get the same result with both versions
+    def testErrorRate(self):
+        self.assertEqual(getErrorRate(self.puzzleInput[1:3], self.puzzleInput[-1]), getErrorRateN(self.puzzleInput[0], self.puzzleInput[-1]))
+
+    def testValidTix(self):
+        self.assertEqual(validTix(self.testInput[0][0], self.testInput[0][-1]), [[7,3,47]])
+        self.assertEqual(validTix(self.testInput[1][0], self.testInput[1][-1]), [[3,9,18],[15,1,5],[5,14,9]])
--- 16/translation.py
+++ 16/translation.py
@@ -8,6 +8,7 @@
 """
 
 import re
+from pathlib import Path
 
 # Split on 2\n's
 # For first group, want to grab name and ranges and place
@@ -30,19 +31,142 @@ def parseInput(inp):
 
     fieldRules = dict()
     for line in rules.split("\n"):
+        # Grab name of field, and the 2 endpoints of both ranges
         field, range1s, range1e, range2s, range2e = re.findall("^(.*): ([0-9]+)\-([0-9]+) or ([0-9]+)\-([0-9]+)$", line)[0]
         fieldRules[field] = [[int(range1s), int(range1e)], [int(range2s), int(range2e)]]
 
+        # Since all ranges of fields overlap in puzzle input,
+        # we can just compare mins/maxes of ranges
         group1R = [min(group1R[0], int(range1s)), max(group1R[1], int(range1e)) ]
         group2R = [min(group2R[0], int(range2s)), max(group2R[1], int(range2e)) ]
 
+    # For the next group, ignore the "your ticket" text
+    # and split the next line at commas and place into a list
+    # Then convert each element into an int
     myVals = list(map(int, yourTix.split("\n")[1].split(",")))
 
+    # Similarly here we can ignore "nearby tickets"
+    # and do the same process for each other ticket,
+    # creating a list of all other ticket data
     otherTix = otherTix.strip().split("\n")[1:]
     for i in range(len(otherTix)):
         otherTix[i] = list(map(int, otherTix[i].split(",")))
 
     return (fieldRules, group1R, group2R, myVals, otherTix)
-    
+
+# Check if a value is valid
+def isValid(range1, range2, val):
+    return (range1[0] <= val <= range1[1]) or (range2[0] <= val <= range2[1])
+
+############### Pt. 1 ######################
+
+# For puzzle input
+def getErrorRate(ranges, tickets):
+    errorRate = 0
+
+    # Since we have two giant ranges to check for our puzzle
+    # input, we don't need to go through every field checking
+    # their individual ranges
+    for ticket in tickets:
+        for val in ticket:
+
+            if not isValid(*ranges, val):
+                errorRate += val
+
+    return errorRate 
+
+# For all other input
+def getErrorRateN(fields, tickets):
+    errorRate = 0
+
+    for ticket in tickets:
+        for val in ticket:
+            valid = False
+            for field in fields.values():
+                # print(field)
+
+                if isValid(*field,val):
+                
+                    valid = True
+                    break
+
+            if not valid: errorRate += val
+
+    return errorRate
+
+############### Pt. 2 #######################
+
+# For this one, we're going to be using indices
+# Start by making a dict of sets for each field index
+# Each column index will map to a set of each valid field description
+# Since we will have cases where we'll need deduction to work out
+# valid field mappings, we want to find all valid fields for a column
+# We'll pass through each ticket, removing possibilities for a field
+# every time there's an invalid check
+
+# For this to work though we first need to remove all invalid tickets
+
+# We're then going to start with the sets with least elements, work 
+# out their field, then remove said field from the other sets
+# to narrow down possibilities. Rince and repeat until all sets
+# have one element - a unique field.
+
+# Think of it like a topological sort-like problem
+
+# Getting rid of invalid tickets
+def validTix(fields, tickets):
+    tikNum = 0
+
+    # While since we're deleting as we go
+    while tikNum < len(tickets):
+
+        for val in tickets[tikNum]:
+            valid = False
+
+            for field in fields.values():
+
+                # Confirmed valid, don't need to check further
+                if isValid(*field,val):
+                    valid = True
+                    break
+
+            if not valid: break
+
+        # Remove a ticket if it's invalid
+        # increment the count otherwise
+        if valid: tikNum += 1
+        else: del tickets[tikNum]
+
+    return tickets
+
+def pt2(inp):
+    # We don't need the all encompassing ranges we used before
+    fieldInfo, _, _, myTicket, otherTickets = inp
+
+    pSets = { x: set(fieldInfo.keys()) for x in range(len(myTicket)) }
 
 
+
+    # Remember to include own ticket in deduction
+    # for ticket in otherTickets:
+
+        # for col in range(len(myTicket)):
+
+
+
+inp = parseInput(Path('./input/puzzle_input').read_text())
+
+# Pt. 1
+# print(getErrorRate(inp[1:3], inp[-1]))
+
+# Although this may get the correct answers,
+# there's a lot of unuaccounted for edge cases.
+# E.g. the definition of a valid ticket is one
+# where each field is valid for some field definition.
+# But what this doesn't account for is if more than one
+# field is valid for only a single definition, yet
+# it's the same one as another field with a single
+# valid definition.
+
+# Pt. 2
+print(pt2(inp))
